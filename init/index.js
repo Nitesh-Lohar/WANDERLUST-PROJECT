7b1070a94f1236efc45f1fe2f5bbf30987c6350f
Nitesh Lohar <lohar.nitesh.nl99@gmail.com>
2026-04-16 23:53:55 +0530

    Authorization for listings

--- init/index.js
+++ init/index.js
@@ -1,6 +1,6 @@
-const mongoose =require('mongoose');
-const initData = require('./data.js');
-const Listing = require('../models/listing.js');
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.model.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -16,10 +16,14 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-const intiDB =async()=>{
-    await Listing.deleteMany({});
-    await Listing.insertMany(initData.data);
-    console.log("Data was initialized");
-}
+const intiDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "69e0d5aa17ccda9b5ce00c47",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("Data was initialized");
+};
 
 intiDB();
--- routes/listing.route.js
+++ routes/listing.route.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const wrapAsync = require("../utils/wrapAsync.js");
@@ -6,8 +6,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const { listingSchema } = require("../joiSchema.js");
 const Listing = require("../models/listing.model.js");
-
-
+const { isLoggedIn, isOwner } = require("../authMiddleware.js");
 
 // Validation Listing Middleware
 const validateListing = (req, res, next) => {
@@ -30,18 +29,20 @@ router.get(
 );
 
 // New form Route
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
   res.render("listings/new.ejs");
 });
 
 // Create Route
 router.post(
   "/",
+  isLoggedIn,
   validateListing,
   wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
     await newListing.save();
-    req.flash("success","New Listing Created")
+    req.flash("success", "New Listing Created");
     res.redirect("/listings");
   }),
 );
@@ -51,7 +52,12 @@ router.get(
   "/a/:id",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id)
+      .populate("reviews")
+      .populate("owner");
+    if (!listing) {
+      req.flash("error", "Not Exist");
+    }
     res.render("listings/show.ejs", { listing });
   }),
 );
@@ -59,6 +65,8 @@ router.get(
 // Edit form Route
 router.get(
   "/:id/edit",
+  isLoggedIn,
+  isOwner,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
@@ -69,11 +77,13 @@ router.get(
 // Update Route
 router.put(
   "/:id",
+  isLoggedIn,
+  isOwner,
   validateListing,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    req.flash("success","Listing Updated")
+    req.flash("success", "Listing Updated");
     res.redirect(`/listings/a/${id}`);
   }),
 );
@@ -81,10 +91,12 @@ router.put(
 // Destroy Route
 router.delete(
   "/:id",
+  isLoggedIn,
+  isOwner,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndDelete(id);
-    req.flash("success","Listing deleted successfully!")
+    req.flash("success", "Listing deleted successfully!");
     res.redirect("/listings");
   }),
 );
